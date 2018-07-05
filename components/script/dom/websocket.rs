0a4123fab9fe360a71f7a4b5d156fe7e1ceb8ae8
Peter Hrvola <peter.hrvola@hotmail.com>
2018-07-05 10:35:40 +0200

    removed unnecessary pub functions

--- components/script/dom/websocket.rs
+++ components/script/dom/websocket.rs
@@ -69,7 +69,7 @@ mod close_code {
     pub const TLS_FAILED: u16 = 1015;
 }
 
-pub fn close_the_websocket_connection<TH: TypeHolderTrait>(
+fn close_the_websocket_connection<TH: TypeHolderTrait>(
     address: Trusted<WebSocket<TH>>,
     task_source: &NetworkingTaskSource<TH>,
     canceller: &TaskCanceller,
@@ -85,7 +85,7 @@ pub fn close_the_websocket_connection<TH: TypeHolderTrait>(
     task_source.queue_with_canceller(close_task, &canceller).unwrap();
 }
 
-pub fn fail_the_websocket_connection<TH: TypeHolderTrait>(
+fn fail_the_websocket_connection<TH: TypeHolderTrait>(
     address: Trusted<WebSocket<TH>>,
     task_source: &NetworkingTaskSource<TH>,
     canceller: &TaskCanceller,
